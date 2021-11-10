0651735285cae1db276e752bc6d9658f874c5f62
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2021-11-10 15:51:28 -0500

    Change project organization and add first test

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,18 @@
+import pytest
+from numlab.tokenizer import Tokenizer
+import re
+
+@pytest.fixture
+def tokenizer():
+    return Tokenizer()
+
+
+def test_add_pattern(tokenizer: Tokenizer):
+    ttype = "TOKEN_TYPE"
+    patt = r"[a+]"
+
+    tokenizer.add_pattern(ttype, patt)
+
+    assert ttype in tokenizer._token_patterns
+    assert tokenizer._token_patterns[ttype] == re.compile(patt)
+
