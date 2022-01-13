50d9f761fec4ed2c348d7642a21ca45423fa980a
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-01-13 02:32:05 -0500

    Add visitor general class

---
+++ numlab/lang/visitor.py
@@ -0,0 +1,57 @@
+"""
+This module contains the Visitor class.
+
+The Visitor class contains a decorator ``visitor`` that is intended to be
+used as a decorator for methods.
+
+Example:
+    >>> class TypeCollector:
+    ...     visitor = Visitor().visitor
+    ...
+    ...     @visitor
+    ...     def visit(self, node: Program):
+    ...         # some code
+    ...
+    ...     @visitor
+    ...     def visit(self, node: Function):
+    ...         # some code
+"""
+
+from inspect import signature
+from typing import Callable, Dict, Tuple
+
+
+class Visitor:
+    """
+    Visitor class.
+
+    A visitor instance must be creater for each class that will decorate
+    methods with the ``visitor`` decorator.
+    """
+
+    def __init__(self):
+        self.cases: Dict[Tuple[type], Callable] = {}
+
+    def visitor(self, func):
+        """
+        Decorator for methods.
+
+        The decorator is intended to be used as a decorator for methods.
+        The method will be registered in the ``cases`` dictionary.
+        """
+        func_sig = signature(func)
+        case_key = tuple(p.annotation for p in func_sig.parameters.values())[1:]
+        if case_key in self.cases:
+            raise ValueError(f"Duplicate visitor: {case_key}")
+
+        def wrapper(*args, **kwargs):
+            case_key = tuple(type(arg) for arg in args[1:])
+            if case_key not in self.cases:
+                raise ValueError(f"No {type(args[0]).__name__} visitor for {case_key}")
+            case_func = self.cases[case_key]
+            return case_func(*args, **kwargs)
+
+        wrapper.__name__ = func.__name__
+        wrapper.__doc__ = func.__doc__
+        self.cases[case_key] = func
+        return wrapper
