5d6a3b44f31c283fe361c859286abf349044051f
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-01-15 12:32:07 -0500

    Add logging format

--- numlab/__init__.py
+++ numlab/__init__.py
@@ -1,3 +1,10 @@
+import logging
 import numlab.nlre
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%H:%M:%S'
+)
 
 __all__ = ["nlre"]
