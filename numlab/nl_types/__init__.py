8bfcc929e7d0e708464d02e074a2882f02c669c3
Robegr42 <robegr42@gmail.com>
2022-01-27 17:07:55 -0500

    Add built in types to __init__

--- numlab/nl_types/__init__.py
+++ numlab/nl_types/__init__.py
@@ -2,3 +2,10 @@ from numlab.nl_types.nl_object import nl_object
 from numlab.nl_types.nl_float import nl_float
 from numlab.nl_types.nl_int import nl_int
 from numlab.nl_types.nl_bool import nl_bool
+from numlab.nl_types.nl_string import nl_string
+from numlab.nl_types.nl_dict import nl_dict
+from numlab.nl_types.nl_list import nl_list
+from numlab.nl_types.nl_tuple import nl_tuple
+from numlab.nl_types.nl_set import nl_set
+from numlab.nl_types.nl_func import nl_func
+from numlab.nl_types.nl_class import nl_class
