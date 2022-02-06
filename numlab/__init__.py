0c5399b4aa6b8faebbeb7e6be0071094d95e380d
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-02-06 10:57:16 -0500

    Add version on init file

--- numlab/__init__.py
+++ numlab/__init__.py
@@ -1,10 +1,13 @@
 import logging
+
 import numlab.nlre
 
 logging.basicConfig(
     level=logging.WARNING,
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    datefmt='%H:%M:%S'
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
 )
 
 __all__ = ["nlre"]
+
+__version__ = "0.1.0"
